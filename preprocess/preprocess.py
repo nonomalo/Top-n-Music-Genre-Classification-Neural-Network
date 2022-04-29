3317b9723f14460e8c035659a3212bb4106b7a3b
somesyd <sydneysomerfield@gmail.com>
2022-04-29 15:03:31 -0700

    removed track segmenting and updated track processing to match Brooks example

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -30,33 +30,8 @@ genre_dict = {
 }
 
 
-def get_mfcc(signal, start, end, sr, n_mfcc=13, n_fft=20, hop_length=512):
-    '''
-    Returns the mfcc sequence for the segment of signal between start and end
-    :param signal: audio as a floating point time series
-    :param start: start index for the signal segment
-    :param end: end index for the signal segment
-    :param sr: sampling rate
-    :param n_mfcc: number of mfccs to return
-    :param n_fft: length of the windowed signal after padding with zeros
-    :param hop_length: number of audio samples between adjacent STFT columns
-    :return: mfcc sequence as numpy ndarray
-    '''
-
-    # apply short-term Fourier transform
-    stft = np.abs(librosa.stft(signal[start:end], n_fft=n_fft, hop_length=hop_length))
-
-    # convert stft to spectrogram on mel-scale
-    mel_spectrogram = librosa.feature.melspectrogram(S=stft ** 2, sr=sr)
-    mel_spectrogram = librosa.power_to_db(mel_spectrogram)
-
-    # generate mfcc for audio track (mel-frequency cepstral coefficients)
-    mfcc = librosa.feature.mfcc(S=mel_spectrogram, n_mfcc=n_mfcc)
-    return mfcc
-
-
-def process_track(file_path, sample_info, genre_index):
-    '''
+def process_track(file_path, sample_info):
+    """
     Splits an audio file into segments and creates an mfcc sequence for
     each segment.
     :param file_path: file path to audio track (string)
@@ -65,35 +40,28 @@ def process_track(file_path, sample_info, genre_index):
     'samples_per_track', 'n_mfcc', 'n_fft', and 'hop_length'
     :param genre_index: genre index for track
     :return: list of sequential mfccs for the audio file
-    '''
+    """
 
     # load audio file as floating point time series
     signal, sr = librosa.load(file_path, sr=sample_info['sample_rate'])
 
-    # to store mfcc segment data
-    track_mfcc_list = []
-    genre_list = []
+    # apply short-term Fourier transform
+    stft = np.abs(librosa.stft(signal, n_fft=sample_info['n_fft'], hop_length=sample_info['hop_length']))
 
-    # break the signal up into segments
-    for num in range(sample_info['number_of_segments']):
+    # convert stft to spectrogram on mel-scale
+    mel_spectrogram = librosa.feature.melspectrogram(S=stft ** 2, sr=sr)
+    db = librosa.power_to_db(mel_spectrogram)  # apply discrete cosine transform
 
-        # establish start and end indices for the segment
-        start = sample_info['samples_per_segment'] * num
-        end = start + sample_info['samples_per_track']
+    # generate mfcc for audio track (mel-frequency cepstral coefficients)
+    mfcc = librosa.feature.mfcc(S=db, n_mfcc=sample_info['n_mfcc'])
 
-        mfcc = get_mfcc(signal, start=start, end=end,
-                        sr=sample_info['sample_rate'],
-                        n_mfcc=sample_info['n_mfcc'],
-                        n_fft=sample_info['n_fft'],
-                        hop_length=sample_info['hop_length'])
+    # add dimension representing channels (to mimic image tensors)
+    mfcc = np.expand_dims(mfcc, -1)
 
-        track_mfcc_list.append(mfcc.tolist())
-        genre_list.append(genre_index)
-
-    return track_mfcc_list, genre_list
+    return mfcc
 
 
-def save_mfcc(dataset_path, audio_files_dir_path, json_path):
+def process_track_list(dataset_path, audio_files_dir_path, json_path):
 
     # dictionary to store data
     data = {
@@ -109,12 +77,7 @@ def save_mfcc(dataset_path, audio_files_dir_path, json_path):
                    'track_duration': 30,
                    'n_mfcc': 20,
                    'n_fft': 2048,
-                   'hop_length': 512,
-                   'number_of_segments': 5}
-
-    # calculate additional information needed for processing
-    sample_info['samples_per_track'] = sample_info['sample_rate'] * sample_info['track_duration']
-    sample_info['samples_per_segment'] = int(sample_info['samples_per_track'] / sample_info['number_of_segments'])
+                   'hop_length': 1024}
 
     # create df from csv file and shuffle the data
     df = pd.read_csv(dataset_path)
@@ -123,6 +86,7 @@ def save_mfcc(dataset_path, audio_files_dir_path, json_path):
     # create genre array that maps to the track index
     top_genres_array = df['top_genre_id'].to_numpy()
     genre_labels = [genre_num_dict[genre_id] for genre_id in top_genres_array]
+    data['labels'] = genre_labels
 
     count = 0
 
@@ -131,7 +95,7 @@ def save_mfcc(dataset_path, audio_files_dir_path, json_path):
 
     # # for TESTING
     # for index in range(0, 5):
-        # file = file_list_array[index]
+    #     file = file_list_array[index]
 
     # loop through files in the dataframe
     for index, file in enumerate(file_list_array):
@@ -140,9 +104,8 @@ def save_mfcc(dataset_path, audio_files_dir_path, json_path):
         file = audio_files_dir_path + file
 
         # save mfccs and corresponding genre labels
-        mfcc_list, genre_list = process_track(file, sample_info, genre_labels[index])
-        data['mfcc'].extend(mfcc_list)
-        data['labels'].extend(genre_list)
+        mfccs = process_track(file, sample_info)
+        data['mfcc'].append(mfccs.tolist())
 
         # display count as we process
         count += 1
@@ -174,4 +137,4 @@ if __name__ == "__main__":
     # save dataframe to csv file
     sample_df.to_csv("csv/small_sample.csv")
 
-    save_mfcc("csv/small_sample.csv", args.audio_root_directory, args.json_file_path)
+    process_track_list("csv/small_sample.csv", args.audio_root_directory, args.json_file_path)
