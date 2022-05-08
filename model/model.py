b72f59462d5a44d86e89db818ab809c38f4c1a6d
Brooks Burns <brooks@brooks-desktop.Home>
2022-05-08 08:58:31 -0700

    Implement incremental training to reduce memory overhead

--- model/model.py
+++ model/model.py
@@ -1,10 +1,11 @@
 """Builds, trains, and evaluates a 2D convolutional neural network
 model on a dataset for music genre prediction.
 
-CL: python3 model.py json-filepath ...
+Usage: python3 model.py json-filepath ...
 """
 
 import json
+import random
 import sys
 from typing import Sequence, Tuple
 
@@ -19,37 +20,45 @@ TIME = 1
 FEATURES = 2
 CHANNELS = 3
 
+MAPPING_DATA = "mapping"
+INPUT_DATA = "mfcc"
+LABEL_DATA = "labels"
 
-def load_data(paths: Sequence[str]) -> Tuple[np.array, np.array, np.array]:
-    """Loads JSON files associated with dataset. JSON files must be structured
-    identically.
 
-    :param paths: array with file paths
-    :return: arrays associated with mappings, features, and labels, respectively
+def verify_arguments() -> None:
+    """Performs initial checks associated with file paths before
+    building and training model.
+
+    :return: None
     """
-    mappings = []
-    inputs = []
-    labels = []
-
     try:
-        for i, path in enumerate(paths):
-            with open(path, "r") as file:
+        for arg in sys.argv[1:]:
+            with open(arg, "r") as file:
                 data = json.load(file)
 
-            # Identical mappings between files expected
-            if i == 0:
-                mappings.extend(data["mapping"])
-    
-            inputs.extend(data["mfcc"])
-            labels.extend(data["labels"])
-    except FileNotFoundError:
-        sys.exit(f"'{path}' is an invalid file path")
+            data[MAPPING_DATA]
+            data[INPUT_DATA]
+            data[LABEL_DATA]
     except IndexError:
-        sys.exit(f"'{path}' has an incompatible JSON structure")
-    
-    mappings = np.array(mappings)
-    inputs = np.array(inputs)
-    labels = np.array(labels)
+        sys.exit(f"Usage: python3 {sys.argv[0]} json-filepath ...")
+    except FileNotFoundError:
+        sys.exit(f"'{arg}' is an invalid file path")
+    except KeyError:
+        sys.exit(f"'{arg}' has an incompatible JSON structure")
+
+
+def load_data(path: str) -> Tuple[np.array, np.array, np.array]:
+    """Loads JSON file associated with dataset.
+
+    :param path: file path
+    :return: arrays associated with mappings, features, and labels, respectively
+    """
+    with open(path, "r") as file:
+        data = json.load(file)
+
+    mappings = np.array(data[MAPPING_DATA])
+    inputs = np.array(data[INPUT_DATA])
+    labels = np.array(data[LABEL_DATA])
     return mappings, inputs, labels
 
 
@@ -72,6 +81,24 @@ def preprocess_data(inputs: Sequence[int]) -> np.array:
     inputs = np.reshape(inputs, newshape=(num_samples, time_steps, num_features))
     inputs = np.expand_dims(inputs, CHANNELS)
     return inputs
+
+
+def initialize_model(
+    input_shape: Sequence[int],
+    num_labels: int
+) -> tf.keras.Model:
+    """Builds and configures model for training.
+
+    :param input_shape: feature data dimensionality
+    :param num_labels: number of all possible output categories
+    :return: trainable model
+    """
+    model = build_model(input_shape, num_labels)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002),
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  metrics=["accuracy"],
+                  )
+    return model
 
 
 def build_model(
@@ -130,13 +157,17 @@ def display_training_metrics(history: tf.keras.callbacks.History) -> None:
 
 def evaluate_model(
     model: tf.keras.Model,
+    mappings: Sequence[int],
     inputs: Sequence[int],
     labels: Sequence[int]
 ) -> None:
-    """Uses trained model to make predictions on test data.
+    """Uses trained model to make predictions on test data. Accuracy
+    on entire test dataset is displayed and confidence values are
+    displayed on a subset of the test dataset.
 
     :param model: trained model
-    :param inputs: test input
+    :param mappings: mappings data
+    :param inputs: test inputs
     :param labels: test labels
     :return: None
     """
@@ -144,51 +175,53 @@ def evaluate_model(
     correct = sum(predicted == labels)
     accuracy = correct / len(labels)
     print(f"Test set accuracy: {accuracy:.0%}")
+    
+    for _ in range(10):
+        i = random.randrange(len(inputs) - 1)
+        prediction = model(np.expand_dims(inputs[i], SAMPLES))
+        plt.bar(mappings, tf.nn.softmax(prediction[0]))
+        plt.title(f'Predictions for "{mappings[labels[i]]}"')
+        plt.xlabel("Labels")
+        plt.ylabel("Confidence")
+        plt.show()
 
 
 def main() -> None:
     """Builds, trains, and evaluates a 2D convolutional neural network
-    model on a dataset for music genre prediction.
+    model on a dataset for music genre prediction. Training is done
+    incrementally to reduce memory overhead.
 
     :return: None
     """
-    dataset_paths = []
+    verify_arguments()
 
-    try:
-        for arg in sys.argv[1:]:
-            dataset_paths.append(arg)
-    except IndexError:
-        sys.exit(f"Usage: python3 {sys.argv[0]} json-filepath ...")
+    for i, path in enumerate(sys.argv[1:], start=1):
+        mappings, inputs, labels = load_data(path)
+        inputs = preprocess_data(inputs)
+        
+        train_inputs, remaining_inputs, \
+            train_labels, remaining_labels = train_test_split(inputs,
+                                                            labels,
+                                                            train_size=0.9
+                                                            )
+        validate_inputs, test_inputs, \
+            validate_labels, test_labels = train_test_split(remaining_inputs,
+                                                            remaining_labels,
+                                                            test_size=0.5
+                                                            )
 
-    mappings, inputs, labels = load_data(dataset_paths)
-    inputs = preprocess_data(inputs)
-    
-    train_inputs, remaining_inputs, \
-        train_labels, remaining_labels = train_test_split(inputs,
-                                                          labels,
-                                                          train_size=0.9
-                                                          )
-    validate_inputs, test_inputs, \
-        validate_labels, test_labels = train_test_split(remaining_inputs,
-                                                        remaining_labels,
-                                                        test_size=0.5
-                                                        )
+        if i == 1:
+            input_shape = (inputs.shape[TIME], inputs.shape[FEATURES], inputs.shape[CHANNELS])
+            model = initialize_model(input_shape, len(mappings))
+            model.summary()
 
-    input_shape = (inputs.shape[TIME], inputs.shape[FEATURES], inputs.shape[CHANNELS])
-    model = build_model(input_shape, len(mappings))
-
-    model.summary()
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002),
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-                  metrics=["accuracy"],
-                  )
-
-    history = model.fit(train_inputs, train_labels, batch_size=32, epochs=35,
-                        validation_data=(validate_inputs, validate_labels),
-                        callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=3)
-                        )
+        history = model.fit(train_inputs, train_labels, batch_size=32, epochs=35,
+                            validation_data=(validate_inputs, validate_labels),
+                            callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=3)
+                            )
+        print(f"Finished training on dataset {i}/{len(sys.argv[1:])}")
     display_training_metrics(history)
-    evaluate_model(model, test_inputs, test_labels)
+    evaluate_model(model, mappings, test_inputs, test_labels)
 
 
 if __name__ == "__main__":
