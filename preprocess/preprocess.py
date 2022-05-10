e263faa63ad5df27680336e004e6b2cc40d41f1b
somesyd <sydneysomerfield@gmail.com>
2022-05-10 15:26:23 -0700

    added: flask app

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -8,29 +8,29 @@ import math
 import os
 
 # # MEDIUM DATASET
-genre_dict = {
-    'International': 0, 'Blues': 1, 'Jazz': 2,
-    'Classical': 3, 'Old-Time / Historic': 4,
-    'Country': 5, 'Pop': 6, 'Rock': 7,
-    'Easy Listening': 8, 'Soul-RnB': 9,
-    'Electronic': 10, 'Folk': 11, 'Spoken': 12,
-    'Hip-Hop': 13, 'Experimental': 14, 'Instrumental': 15
-}
-mapping = ['International', 'Blues', 'Jazz', 'Classical',
-           'Old-Time / Historic', 'Country', 'Pop', 'Rock',
-           'Easy Listening', 'Soul-RnB', 'Electronic',
-           'Folk', 'Spoken', 'Hip-Hop', 'Experimental',
-           'Instrumental']
+# genre_dict = {
+#     'International': 0, 'Blues': 1, 'Jazz': 2,
+#     'Classical': 3, 'Old-Time / Historic': 4,
+#     'Country': 5, 'Pop': 6, 'Rock': 7,
+#     'Easy Listening': 8, 'Soul-RnB': 9,
+#     'Electronic': 10, 'Folk': 11, 'Spoken': 12,
+#     'Hip-Hop': 13, 'Experimental': 14, 'Instrumental': 15
+# }
+# mapping = ['International', 'Blues', 'Jazz', 'Classical',
+#            'Old-Time / Historic', 'Country', 'Pop', 'Rock',
+#            'Easy Listening', 'Soul-RnB', 'Electronic',
+#            'Folk', 'Spoken', 'Hip-Hop', 'Experimental',
+#            'Instrumental']
 
 # SMALL DATASET
-# genre_dict = {
-#     'Electronic': 0, 'Experimental': 1, 'Folk': 2,
-#     'Hip-Hop': 3, 'Instrumental': 4,
-#     'International': 5, 'Pop': 6, 'Rock': 7
-# }
-# mapping = ['Electronic', 'Experimental', 'Folk',
-#            'Hip-Hop', 'Instrumental',
-#            'International', 'Pop', 'Rock']
+genre_dict = {
+    'Electronic': 0, 'Experimental': 1, 'Folk': 2,
+    'Hip-Hop': 3, 'Instrumental': 4,
+    'International': 5, 'Pop': 6, 'Rock': 7
+}
+mapping = ['Electronic', 'Experimental', 'Folk',
+           'Hip-Hop', 'Instrumental',
+           'International', 'Pop', 'Rock']
 
 
 def main():
